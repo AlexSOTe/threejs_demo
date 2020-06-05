c5025af6851a71abb9721e52efffe52bda70541b
AlexSOTe <alexsote@outlook.com>
2020-06-05 11:50:43 +0800

    Revert "该称class"
    This reverts commit 7de9dabb34ed8a73d645e5b535b246a2349e2c61.

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,18 @@
-import './libs/plugins';
-import { AppConfig } from './app.config';
-import camera from './components/Camera';
-import renderer from './components/Renderer';
-import { MainScene } from './scenes/main';
+import './libs/plugins'
+import { AppConfig } from './app.config'
+import camera from './components/Camera'
+import renderer from './components/Renderer'
+import { MainScene } from './scenes/main'
 
 window.addEventListener('load', () => {
-  AppConfig();
+  AppConfig()
 
-  let mainScene: MainScene = new MainScene();
-  mainScene.Init();
-  mainScene.Start();
-
-  document.querySelector("#start")?.addEventListener("click", () => {
-    mainScene.Start();
-  })
-  document.querySelector("#pause")?.addEventListener("click", () => {
-    mainScene.Pause();
-  })
-  // 有问题
-  //document.querySelector("#reset")?.addEventListener("click", () => {
-  //  mainScene.Reset();
-  //})
-
+  MainScene()
 
   window.addEventListener('resize', () => {
     // 以下三行保证 resize 时场景保持不变形
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
   }, false)
 })
